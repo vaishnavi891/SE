b662344f943e286c5e8fd941162697c280ac8af9
suzakijun1 <dyong1997@gmail.com>
2023-05-08 14:35:15 -0400

    index

--- seeds/index.js
+++ seeds/index.js
@@ -3,9 +3,18 @@ const sequelize = require("../config/connection");
 const userData = require("./userData.json");
 const calendarData = require("./calendarData.json");
 const dayData = require("./dayData.json");
+const scoreData = require("./scoreData.json");
+
+const { User, Calendar, Day, Score } = require("../models");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+  });
+  await Calendar.bulkCreate(calendarData);
+  await Day.bulkCreate(dayData);
+  await Score.bulkCreate(scoreData);
 };
 
 seedDatabase;
