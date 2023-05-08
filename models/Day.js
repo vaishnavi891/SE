325715603d84c1873ae9b8ec366e93254deb70c2
suzakijun1 <dyong1997@gmail.com>
2023-05-08 15:14:40 -0400

    day fixing

--- models/Day.js
+++ models/Day.js
@@ -1,41 +1,41 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
-class Day extends Model { }
+class Day extends Model {}
 
 Day.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        day_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            isUnique: false,
-        },
-        day_of_the_month: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            isUnique: false,
-        },
-        calender_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'calender',
-                key: 'id',
-            },
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'day',
-    }
+    day_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      isUnique: false,
+    },
+    day_of_the_month: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      isUnique: false,
+    },
+    calender_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "calendar",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "day",
+  }
 );
 
 module.exports = Day;
