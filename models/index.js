e957d73f44ed716a9544a6bb85d92da5a5d6724a
suzakijun1 <dyong1997@gmail.com>
2023-05-08 14:22:10 -0400

    indexmodel

--- models/index.js
+++ models/index.js
@@ -1,6 +1,20 @@
-const User = require('./User');
+const User = require("./User");
+const Calendar = require("./Calendar");
+const Day = require("./Day");
+const Score = require("./Score");
 
+User.hasMany(Calendar, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
 
+Calendar.hasMany(Day, {
+  foreignKey: "calendar_id",
+  onDelete: "CASCADE",
+});
 
+Score.belongsTo(Day, {
+  foreignKey: "day_id",
+});
 
 module.exports = { User };
