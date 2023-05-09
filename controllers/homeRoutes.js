5f6353f77c3e1332b473e06d05edaf9b9b529843
suzakijun1 <dyong1997@gmail.com>
2023-05-09 12:46:40 -0400

    homeroute

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,13 +1,71 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { User, Day, Calendar, Score } = require("../models");
+const withAuth = require("../utils/auth");
 
-
-//Gets all post in db and displays them 
-router.get('/', async (req, res) => {
-    try {
-        res.render('about');
-    } catch (err) {
-        res.status(400).json(err);
-    }
+//Gets all post in db and displays them
+router.get("/", async (req, res) => {
+  try {
+    res.render("about");
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-module.exports = router
+//dashboard
+router.get("/dashboard", withAuth, async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      include: [
+        {
+          model: Calendar,
+          include: [
+            {
+              model: Day,
+              include: [Score],
+            },
+          ],
+        },
+      ],
+    });
+    //convert the Sequelize model instances to plain JavaScript objects (allows the use of handlebar)
+    const userInfo = userInfo.map((data) => data.get({ plain: true }));
+
+    res.render("dashboard", {
+      userInfo,
+      loggedIn: req.session.loggedIn,
+    });
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//login
+router.get("/login", async (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.loggedIn) {
+    res.redirect("/profile");
+    return;
+  }
+  res.render("login");
+});
+
+//resources
+router.get("/resources", async (req, res) => {
+  try {
+    res.render("about");
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//journals
+router.get("/journals", async (req, res) => {
+  try {
+    res.render("journals");
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
