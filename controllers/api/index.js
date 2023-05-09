7441e4f7f061324c51b07b4ba543f1ecfbb0cb23
Sami <coolsweetsami@gmail.com>
2023-05-09 10:44:17 -0400

    updates to index route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const questionsRoutes = require('./questionsRoute');
 
 router.use('/users', userRoutes);
+router.use('/questions', questionsRoutes);
 
 module.exports = router;
