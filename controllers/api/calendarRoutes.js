12d333e64bed091474d3d223826a36bb78de3b7b
David Peguero <david.peguero.cancel@gmail.com>
2023-05-09 10:41:04 -0400

    Added get route with id parameter

--- controllers/api/calendarRoutes.js
+++ controllers/api/calendarRoutes.js
@@ -17,6 +17,27 @@ router.get('/', async (req, res) => {
     }
 })
 
+//Retrieve calendar with an ID
+router.get('/:id', async (req, res) => {
+    try {
+        const calendarData = await Calendar.findOne({
+            where : {
+                id : req.params.id
+            }
+        })
+
+        if(!calendarData){
+            res.status(404).json({error: 404, message : "Cannot find a Calendar with that id" });
+            return;
+        }
+
+        res.status(200).json(calendarData);
+
+    } catch (error) {
+        res.status(400).json(err);
+    }
+})
+
 router.post('/', async (req, res) => {
     try {
         const calendarData = await Calendar.create(req.body);
