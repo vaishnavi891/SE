40e4d70c8aca65847837d810a50a73988699e98d
Sami <coolsweetsami@gmail.com>
2023-05-09 11:48:13 -0400

    deleted route and json that was not needed

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const questionsRoutes = require('./questionsRoute');
 
 router.use('/users', userRoutes);
-router.use('/checkin', questionsRoutes);
+
 
 module.exports = router;
