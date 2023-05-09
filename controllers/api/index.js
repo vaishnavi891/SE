432f15bb3039e55e37bfbc2e52e7eb90e54bd25c
David Peguero <david.peguero.cancel@gmail.com>
2023-05-09 13:25:52 -0400

    Added logRoutes files with crud functionality

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,11 +3,13 @@ const userRoutes = require("./userRoutes");
 const calendarRoutes = require("./calendarRoutes");
 const scoreRoutes = require("./scoreRoutes");
 const dayRoutes = require("./dayRoutes");
+const logRoutes = require("./logRoutes");
 
 router.use("/users", userRoutes);
 router.use("/calendars", calendarRoutes);
 router.use("/scores", scoreRoutes);
 router.use("/days", dayRoutes);
+router.use("/logs", logRoutes);
 
 
 
