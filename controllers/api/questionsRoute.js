1a3b5de79d4238c5ea3e7182e4e447fa1dee3449
Sami <coolsweetsami@gmail.com>
2023-05-09 11:06:55 -0400

    updates to package.json, added uuid

--- controllers/api/questionsRoute.js
+++ controllers/api/questionsRoute.js
@@ -1,21 +1,34 @@
 const router = require('express').Router();
-const { getQuestions } = require('./questions');
+const path = require('path');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const { v4: uuidv4 } = require('uuid');
+const questions = JSON.parse(fs.readFileSync('./questions.json', 'utf8'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 router.get('/checkin', (req, res) => {
-  const questions = getQuestions();
   res.render('checkin', { questions });
 });
 
-router.post('/checkin', (req, res) => {
-  const answers = req.body;
+app.post('/checkin', (req, res) => {
+  const answers = {
+    id: uuidv4(),
+    timestamp: new Date(),
+    ...req.body
+  };
 
   // Write the answers to a JSON file
-  const dataFilePath = path.join(__dirname, 'scoreData.json');
+  const dataFilePath = path.join(__dirname, 'answers.json');
   const data = JSON.parse(fs.readFileSync(dataFilePath, 'utf8'));
   data.push(answers);
   fs.writeFileSync(dataFilePath, JSON.stringify(data));
 
   res.send('Thanks for your submission!');
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
 });
 
 
