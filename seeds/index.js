e5e6dfc615cbb2f3b015e1d9537618527c1804e3
David Peguero <david.peguero.cancel@gmail.com>
2023-05-09 21:54:04 -0400

    Deleted Calendar model and all references to it

--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,10 @@
 const sequelize = require("../config/connection");
 const userData = require("./userData.json");
-const calendarData = require("./calendarData.json");
 const dayData = require("./dayData.json");
 const scoreData = require("./scoreData.json");
 const logData = require('./logData.json');
 
-const { User, Calendar, Day, Score, Log } = require("../models");
+const { User, Day, Score, Log } = require("../models");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -14,7 +13,6 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
-  await Calendar.bulkCreate(calendarData);
   await Day.bulkCreate(dayData);
   await Score.bulkCreate(scoreData);
   await Log.bulkCreate(logData)
