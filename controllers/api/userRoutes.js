7584b936b0b676070eb68e67c0f2fce948d3daaa
David Peguero <david.peguero.cancel@gmail.com>
2023-05-10 11:29:54 -0400

    Login page is now functional

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -57,9 +57,11 @@ router.post('/login', async (req, res) => {
       
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-      res.json({ user: userData, message: 'You are now logged in!', user_id : req.session.user_id });
     });
-
+    if(req.session.logged_in){
+      console.log(req.session.logged_in);
+    }
+    res.json({ user: userData, message: 'You are now logged in!', user_id : req.session.user_id });
   } catch (err) {
     res.status(400).json(err);
   }
