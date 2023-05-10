b9ceba831cf281f447a0885e068ae631e0ee0bc5
suzakijun1 <dyong1997@gmail.com>
2023-05-10 10:13:35 -0400

    wellbeingfix

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,13 +3,12 @@ const userRoutes = require("./userRoutes");
 const scoreRoutes = require("./scoreRoutes");
 const dayRoutes = require("./dayRoutes");
 const logRoutes = require("./logRoutes");
+const wellbeingRoutes = require("./wellbeingRoutes");
 
 router.use("/users", userRoutes);
 router.use("/scores", scoreRoutes);
 router.use("/days", dayRoutes);
 router.use("/logs", logRoutes);
-
-
+router.use("/wellbeings", wellbeingRoutes);
 
 module.exports = router;
-
