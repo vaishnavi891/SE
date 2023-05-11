c1005901cffb3b86639ccc78013cbdcf5c8ffdbe
brandon6457 <brandon6457@gmail.com>
2023-05-11 14:47:12 -0400

    fixed subtitle to center it in signup

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-const { User, Day, Log, Score } = require("../../models");
+const { User, Day, Score } = require("../../models");
 
 router.get("/", async (req, res) => {
   try {
